2423b0dd202953fa448ab45b90d23c4211a0aaf5
mahmoudmekki <149210160+mmekki96@users.noreply.github.com>
2025-05-05 21:40:41 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "stm32f407xx.h"
+
+int main() {
+	//**LED PD12 initilisation**
+	RCC->AHB1ENR |= (1 << 3);
+	GPIOD->MODER &= ~(3 << (12*2));
+	GPIOD->MODER |= (1 << (12*2));
+	GPIOD->OTYPER &= ~(1 << 12);
+	GPIOD->OSPEEDR |= (3 << (12*2));
+	GPIOD->PUPDR &= ~(3 << (12*2));
+	//**Button PA0 initilisation**
+	RCC->AHB1ENR |= (1 << 0);
+	GPIOA->MODER &= ~(1 << (0*2));
+	GPIOA->PUPDR &= ~(1 << (0*2));
+	
+	while(1) {
+		if (GPIOA->IDR & (1 << 0))
+			GPIOD->ODR |= (1 << 12);
+		else 
+			GPIOD->ODR &= ~(1 << 12);
+	}
+	
+}
